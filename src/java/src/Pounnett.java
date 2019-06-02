7cee91e7e4b42148dd2f5d6ae4fdce7de59c4efe
Riscolso <rriscolso@gmail.com>
2019-06-02 06:32:24 -0500

    trabajado hasta cruce con 2 genes

--- src/java/src/Pounnett.java
+++ src/java/src/Pounnett.java
@@ -18,23 +18,69 @@ public class Pounnett {
         char alelo2A = genesA.get(n).charAt(1);
         char alelo1B = genesB.get(n).charAt(0);
         char alelo2B = genesB.get(n).charAt(1);
-        aux.add(alelo1A+alelo1B+"");
-        aux.add(alelo1A+alelo2B+"");
-        aux.add(alelo2A+alelo1B+"");
-        aux.add(alelo2A+alelo2B+"");
+        aux.add(alelo1A+""+alelo1B);
+        aux.add(alelo1A+""+alelo2B);
+        aux.add(alelo2A+""+alelo1B);
+        aux.add(alelo2A+""+alelo2B);
         return aux;
     }
     
+    
+    /**
+     * Genera la combinación de todos los cruces de los genes A y B
+     * @return Lista con los cruces de los genes.
+     * @deprecated 
+     */
     public ArrayList<String> generarCruce(){
+        int c = (int)Math.pow(2, cantGenes);
+        ArrayList<String> carac = new ArrayList<String>();
+        for(int i=0;i<cantGenes;i++){
+            carac.add((char)('A'+i)+"");
+        }
+        for(int i=0;i<carac.size();i++){
+            String aux = carac.get(i);
+            for(int e=0;e<c/2;e++){
+                
+                
+            }
+            aux.toLowerCase();
+        }
+        
         return genesA;
     }
     
-    float probabilidadAlelo(char c){
-        return (float)1.2;
+    /**
+     * Devuelve una probabilidad respecto a un alelo dado tomando en cuanta si el alelo es domante o no.<br>
+     * Hace uso de la funcion {@link #generarCruce(int)}
+     * @param c Alelo del cual se quiere saber su probabilidad.
+     * @param g El cruce de los genes
+     * @return Probabilidad del Alelo de ser heredado.
+     */
+    float probabilidadAlelo(char c, int g){
+        float j=0;
+        for(String s: generarCruce(g)){
+            float p=0, r=0;
+            if(Character.isUpperCase(s.charAt(0)) || Character.isUpperCase(s.charAt(1))) p=1;
+            if(Character.isUpperCase(c)) r=1;
+            if(p+r==2) j++;
+            else if(p==r) j++;
+        }
+        return (float)j/4;
     }
-    
+    /**
+     * Devuelve una probabilidad de obtener un genotipo respecto a una combinación de genes.<br>
+     * Hace uso de la función {@link #probabilidadAlelo(char) }.
+     * @param s Combinación de genes
+     * @return probabilidad de obtener ese genotipo.
+     */
     float probabilidadGenotipo(String s){
-        return (float)1.2;
+        float p=1;
+        int i=0;
+        for(char c:s.toCharArray()){
+            p*=probabilidadAlelo(c, i);
+            i++;
+        }
+        return p;
     }
 
     public int getCantGenes() {
--- src/java/src/Servlet.java
+++ src/java/src/Servlet.java
@@ -2,6 +2,7 @@ package src;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,9 +24,6 @@ public class Servlet extends HttpServlet {
         for(int i=p.getCantGenes();i<p.getCantGenes()*2;i++){
             p.getGenesB().add(request.getParameter("cb"+i));
         }
-        System.out.println("Cantidad de genes "+p.getCantGenes());
-        System.out.println("Genes de A: "+p.genesA.subList(0, p.getGenesA().size()));
-        System.out.println("Genes de B: "+p.genesA.subList(0, p.getGenesB().size()));
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         out.println("<!DOCTYPE html>");
@@ -34,7 +32,45 @@ public class Servlet extends HttpServlet {
         out.println("<title>Servlet</title>");            
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>Hello, World!</h1>");
+        out.println("Cantidad de genes "+p.getCantGenes());
+        out.println("<br>");
+        out.println("Genes de A: "+p.genesA.subList(0, p.getGenesA().size()));
+        out.println("<br>");
+        out.println("Genes de B: "+p.genesB.subList(0, p.getGenesB().size()));
+        char a = 'a';
+        char aux;
+        for(int i=0;i<p.getCantGenes();i++){
+            aux = (char) (a+i);
+            out.println("<br><br>");
+            out.println("Cuadro "+i+"<br>");
+            ArrayList<String> c = p.generarCruce(i);
+            for(int e=0;e<c.size();e++){
+                out.print(c.get(e)+" ");
+                if(e==p.getCantGenes()-1) out.println("<br>");
+            }
+            out.println("<br>");
+            out.print("Probabilidad de "+aux +" es :"+p.probabilidadAlelo(aux, i)+"<br>");
+            out.print("Probabilidad de "+Character.toUpperCase(aux) +" es :"+p.probabilidadAlelo(Character.toUpperCase(aux),i));
+        }
+        out.print("<br><br>");
+        
+        /*Se deben mostrar las combinaciones de los genes, en el código temporal se muestra la combinación de cuando es Aa y Bb
+        Cuando son 3 genes por ejemplo, se debe mostrar la combinación de
+        "Probabilidad ABC :"
+        "Probabilidad ABc :"
+        "Probabilidad AbC :"
+        "Probabilidad aBC :"
+        "Probabilidad Abc :"
+        "Probabilidad abC :"
+        Y así.....
+        */
+        /*CÓDIGO TEMPORAL*/
+        out.print("Probabilidad AB :"+p.probabilidadGenotipo("AB")+"<br>");
+        out.print("Probabilidad Ab :"+p.probabilidadGenotipo("Ab")+"<br>");
+        out.print("Probabilidad aB :"+p.probabilidadGenotipo("aB")+"<br>");
+        out.print("Probabilidad ab :"+p.probabilidadGenotipo("ab")+"<br>");
+        /*CÓDIGO TEMPORAL*/
+        
         out.println("</body>");
         out.println("</html>");
     }
